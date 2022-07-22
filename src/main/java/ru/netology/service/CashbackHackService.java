2d1fde6ff0d7f774ca4abf58b752651c7ae95495
Kristina0805 <kristya25.95@mail.ru>
2022-07-22 16:15:30 +0500

    new branch

--- src/main/java/ru/netology/service/CashbackHackService.java
+++ src/main/java/ru/netology/service/CashbackHackService.java
@@ -3,9 +3,9 @@ package ru.netology.service;
 
 public class CashbackHackService {
 
-    private final int boundary = 1000;
+    private final int boundary = 1000;   // boundary - граница
 
-    public int remain(int amount) {
+    public int remain(int amount) {       // amount - сумма
         return boundary - amount % boundary;
     }
 }
